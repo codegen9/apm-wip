ee469f38b0f2857eb7f77d8467fa0d5465ca9cce
codegen9 <sahan92@gmail.com>
2017-05-21 18:52:05 +0530

    adds product service and basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { WelcomeComponent }  from './home/welcome.component';
 import { ProductListComponent }  from './products/product-list.component';
+import { ProductDetailComponent }  from './products/product-detail.component';
 import { StarComponent }  from './shared/star.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
+    ProductDetailComponent,
     StarComponent,
     ProductFilterPipe
   ],
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
 
+import { ProductService } from './product-service';
+
 @Component({
     selector: 'pm-products',
     moduleId: module.id,
@@ -14,35 +16,21 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = '';
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[];
+    errorMessage: string;
+
+    constructor(private _productService: ProductService) {
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this._productService.getProducts().subscribe
+        (products => this.products = products,
+         error => this.errorMessage = <any>error
+        );
     }
 
     onRatingClicked(message: string): void {
